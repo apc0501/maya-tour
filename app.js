eb9fe81e5f089cccde7f52b42242107948f601b0
apc0501 <apc0501@gmail.com>
2025-07-18 21:37:31 -0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Maya Tour App');
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.createElement('button');
+  button.textContent = 'Ver promoções';
+  document.body.appendChild(button);
+
+  button.addEventListener('click', function () {
+    alert('Ainda não há promoções disponíveis, mas você será notificado em breve!');
+  });
+});
